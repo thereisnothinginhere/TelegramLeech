fad75d6f65ccec9a42accf5e3f752bf5dbfefe03
thereisnothinginhere <96543392+thereisnothinginhere@users.noreply.github.com>
2023-12-03 12:15:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ links = soup.find_all('a', href=lambda x: x and x.startswith(Site+'index.php?/fo
 
 # subprocess.Popen('git config user.name "GitHub Actions"', shell=True, stdout=subprocess.PIPE)
 # subprocess.Popen('git config user.email "actions@github.com"', shell=True, stdout=subprocess.PIPE)
-
+delete_all()
 # Open the file in append mode to add new magnet links
 with open(filename, "a") as file:
     # start_time = time()
@@ -182,8 +182,8 @@ with open(filename, "a") as file:
                 file.write(magnet + "\n")
                 existing_magnet_links.add(magnet)
 
-        # git_add_process = subprocess.Popen("sh git.sh", shell=True, stdout=subprocess.PIPE)
-        # git_add_process.wait()
+        git_add_process = subprocess.Popen("sh git.sh", shell=True, stdout=subprocess.PIPE)
+        git_add_process.wait()
         
         # git_add_process = subprocess.Popen("git add magnet_links.txt", shell=True, stdout=subprocess.PIPE)
         # git_add_process.wait()
