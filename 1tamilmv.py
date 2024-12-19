d2be524ddcb544e230f7749c360793c956c885e8
thereisnothinginhere <96543392+thereisnothinginhere@users.noreply.github.com>
2024-12-19 13:40:03 +0530

    Add files via upload

--- 1tamilmv.py
+++ 1tamilmv.py
@@ -1,35 +1,43 @@
 from bs4 import BeautifulSoup
-from extensions import seedr_download
+from extensions import delete_all,seedr_download,aria2_download,upload_video
 from time import time
 from seedrcc import Login,Seedr
 import requests
+import os
 
-Username  = "herobenhero2@gmail.com" #@param {type:"string"}
-Password  = "JBD7!xN@oTSkrhKd7Pch" #@param {type:"string"}
+# Username  = "herobenhero2@gmail.com" #@param {type:"string"}
+# Password  = "JBD7!xN@oTSkrhKd7Pch" #@param {type:"string"}
+
+Username  = "herobenhero5@gmail.com" #@param {type:"string"}
+Password  = "zyHyuTfaGiC6:uP" #@param {type:"string"}
+
 chat_id = '-1002068315295'
 
 account = Login(Username, Password)
 account.authorize()
 seedr = Seedr(token=account.token)
 
+CHAT_ID = '-1002068315295' #@param {type:"string"}
+THUMBNAIL_PATH = 'Thumbnail.jpg' #@param {type:"string"}
+
 def get_magnetic_urls(URL):
-  # Send an HTTP request to the web server
-  response = requests.get(URL)
-
-  # Parse the HTML code of the web page
-  soup = BeautifulSoup(response.text, 'html.parser')
-
-  # Find all the <a> elements on the page that have a "magnet" href attribute
-  magnetic_links = soup.find_all('a', href=lambda x: x and x.startswith('magnet:'))
-  magnets=[]
-  # Print the text of each magnetic link
-  for link in magnetic_links:
-    magnets.append(link['href'])
-  return magnets
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    magnetic_links = soup.find_all('a', href=lambda x: x and x.startswith('magnet:'))
+    magnets = [link['href'] for link in magnetic_links]
+    return magnets
 
 
-Site = "https://www.1tamilmv.world/"  # @param {type:"string"}
-filename = "magnet_links.txt"
+Site = "https://www.1tamilmv.ac/"  # @param {type:"string"}
+
+delete_all(seedr)
+
+filename = "magnet_links.txt"  # @param {type:"string"}
+
+# Fetching links
+response = requests.get(Site)
+soup = BeautifulSoup(response.text, 'html.parser')
+links = [link['href'] for link in soup.find_all('a', href=lambda x: x and x.startswith(Site + 'index.php?/forums/topic/'))]
 
 # Load existing magnet links from the file
 try:
@@ -38,44 +46,17 @@ try:
 except FileNotFoundError:
     existing_magnet_links = set()
 
-# Send an HTTP request to the web server
-response = requests.get(Site)
-
-# Parse the HTML code of the web page
-soup = BeautifulSoup(response.text, 'html.parser')
-
-# Find all the links on the page that start with https://www.1tamilmv.autos/index.php?/forums/topic/
-links = soup.find_all('a', href=lambda x: x and x.startswith(Site+'index.php?/forums/topic/') and x.endswith('-0'))
-links = {link.get('href') for link in links if link.get('href') is not None}
-
-# subprocess.Popen('git config user.name "GitHub Actions"', shell=True, stdout=subprocess.PIPE)
-# subprocess.Popen('git config user.email "actions@github.com"', shell=True, stdout=subprocess.PIPE)
-try:
-    # Open the file in append mode to add new magnet links
-    with open(filename, "a") as file:
-        start_time = time()
-        for link in links:
-            magnets = get_magnetic_urls(link)
-            for magnet in magnets:
-                if magnet not in existing_magnet_links:
-                    # Write new magnet links to the file
-                    seedr_download(magnet,seedr,chat_id)
-                    file.write(magnet + "\n")
-                    existing_magnet_links.add(magnet)
-    
-            # git_add_process = subprocess.Popen("sh git.sh", shell=True, stdout=subprocess.PIPE)
-            # git_add_process.wait()
-            # break
-            
-            # git_add_process = subprocess.Popen("git add magnet_links.txt", shell=True, stdout=subprocess.PIPE)
-            # git_add_process.wait()
-    
-            # git_commit_process = subprocess.Popen('git commit -m "Updated"', shell=True, stdout=subprocess.PIPE)
-            # git_commit_process.wait()
-    
-            # elapsed_time = time() - start_time
-            # if elapsed_time > 0.2 * 60 * 60:  # 5 hours in seconds
-            #     print("Stopping script after 2.5 hours.")
-            #     break
-except Exception as e:
-    print("Error Occured :",e)
+# Open the file, write the magnet link, and close it immediately
+with open(filename, "a") as file:
+    for site in sorted(links):
+        magnets = get_magnetic_urls(site)
+        for magnet in reversed(magnets):  # You might want to sort this if needed
+            if magnet not in existing_magnet_links:
+                id, urls = seedr_download(magnet,seedr)
+                for filepath, encoded_url in urls.items():
+                    aria2_download(filepath, encoded_url)
+                    upload_video(chat_id,filepath,THUMBNAIL_PATH)
+                seedr.deleteFolder(id)
+                file.write(magnet + "\n")
+                file.flush()  # Ensure data is written immediately
+                existing_magnet_links.add(magnet)
--- extensions.py
+++ extensions.py
@@ -1,13 +1,54 @@
-from time import sleep
-import subprocess
 import os
-from telegram.ext import Updater
+import subprocess
 import math
+from seedrcc import Login,Seedr
+from time import sleep
 import urllib.parse
-from telegram.error import RetryAfter
+from urllib.parse import unquote
+import requests
 
+# User Inputs
+TELEGRAM_TOKEN = '5942550686:AAEkBVyp0U0zhP3z7ylmw4m2KS-pTD9UyZQ'  # Important secret in caps
+API_HASH = 'e18a786351fb201760b000217ff60500'  # Important secret in caps
+API_ID = '10437406'
 API_SERVER_URL = 'http://localhost:8081/bot'
-TELEGRAM_TOKEN = '5942550686:AAEkBVyp0U0zhP3z7ylmw4m2KS-pTD9UyZQ'
+
+def get_video_duration(file_path):
+    cmd_duration = f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "{file_path}"'
+    output_duration = subprocess.check_output(cmd_duration, shell=True).decode('utf-8').strip()
+    return int(float(output_duration))
+
+def generate_thumbnail(file_path):
+    thumbnail_path = 'Sample.jpg'
+    cmd_thumbnail = f'ffmpeg -i "{file_path}" -ss 00:00:01 -vframes 1 "{thumbnail_path}"'
+    subprocess.call(cmd_thumbnail, shell=True)
+    return thumbnail_path
+
+def upload_video(CHAT_ID, file_path, thumbnail_path=None):
+
+    duration = get_video_duration(file_path)
+    if not thumbnail_path:
+        thumbnail_path = generate_thumbnail(file_path)
+    caption = os.path.basename(file_path)
+
+    if not check_bot_status():
+        return
+
+    print("Uploading Video...")
+    while True:
+        with open(file_path, 'rb') as video, open(thumbnail_path, 'rb') as thumb:
+            files = {'video': video, 'thumb': thumb}
+            data = {'chat_id': CHAT_ID, 'duration': duration, 'caption': caption}
+            response = requests.post(f"{API_SERVER_URL}{TELEGRAM_TOKEN}/sendVideo", files=files, data=data)
+            response_data = response.json()
+            if response_data['ok']:
+                print(f'Video {file_path} sent successfully!')
+                os.remove(file_path)
+                break
+            else:
+                print(response_data)
+                print(f'Failed to send video {file_path}. Retrying in 30 seconds...')
+                sleep(30)
 
 def convert_size(size_bytes):
     """Convert the size in bytes to a more human-readable format."""
@@ -19,83 +60,7 @@ def convert_size(size_bytes):
     s = round(size_bytes / p, 2)
     return f"{s} {size_name[i]}"
 
-def get_video_duration(file_path):
-    cmd = f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "{file_path}"'
-    output = subprocess.check_output(cmd, shell=True).decode('utf-8').strip()
-    return int(float(output))
-
-def generate_thumbnail(video_path, time, thumbnail_path):
-    cmd = f'ffmpeg -i "{video_path}" -ss {time} -vframes 1 "{thumbnail_path}"'
-    subprocess.call(cmd, shell=True)
-
-def send_video_file(file_path, thumbnail_path, chat_id):
-    print(f"Sending {file_path} to Telegram")
-
-    time = '00:00:01'
-    # generate_thumbnail(file_path, time, thumbnail_path)
-
-    if not os.path.exists(file_path):
-        print(f'File {file_path} not found.')
-        return
-
-    updater = Updater(token=TELEGRAM_TOKEN, use_context=True, base_url=API_SERVER_URL)
-    bot = updater.bot
-
-    duration = get_video_duration(file_path)
-    thumbnail_path='Thumbnail.jpg'
-
-    retries = 5  # Adjust the number of retries based on your needs
-    for attempt in range(retries):
-        try:
-            with open(file_path, 'rb') as file, open(thumbnail_path, 'rb') as thumb:
-                bot.send_video(chat_id=chat_id, video=file, duration=duration, thumb=thumb, timeout=999, caption=file_path)
-            print(f'Video {file_path} sent successfully!')
-            os.remove(file_path)
-            break  # Exit the loop if successful
-        except RetryAfter as e:
-            sleep_time = e.retry_after  # Get the required wait time from the exception
-            print(f"Rate limit exceeded. Sleeping for {sleep_time} seconds and retrying.")
-            sleep(sleep_time * 2 ** attempt)  # Exponential backoff
-        except Exception as e:
-            print(f"Error sending video: {e}")
-            sleep(60)
-            # break  # Exit the loop on other errors
-
-def get_audio_duration(file_path):
-    cmd = f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "{file_path}"'
-    output = subprocess.check_output(cmd, shell=True).decode('utf-8').strip()
-    return int(float(output))
-
-def send_audio_file(file_path,chat_id):
-    if not os.path.exists(file_path):
-        print(f'File {file_path} not found.')
-        return
-
-    updater = Updater(token=TELEGRAM_TOKEN, use_context=True, base_url=API_SERVER_URL)
-    bot = updater.bot
-
-    duration = get_audio_duration(file_path)
-
-    with open(file_path, 'rb') as file:
-        bot.send_audio(chat_ibotd=chat_id, audio=file, duration=duration, timeout=999)
-
-    print(f'Audio {file_path} sent successfully!')
-
-def aria2_download(filename, link, chat_id):
-    print(f"Downloading {filename} with {link}")
-
-    command = f"aria2c -o '{filename}' --summary-interval=1 --max-connection-per-server=2 '{link}'"
-
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-    process.wait()
-
-    if ".mp3" in filename:
-        send_audio_file(filename,chat_id)
-    else:
-        send_video_file(filename, filename +'.jpg', chat_id)
-
 def delete_all(seedr):
-    #@title **List All**
     table=seedr.listContents()
     if len(table['torrents']) != 0:
         for torrent in table['torrents']:
@@ -104,50 +69,91 @@ def delete_all(seedr):
         for folder in table['folders']:
             seedr.deleteFolder(folder['id'])
 
-def seedr_download(MagneticURL,seedr, chat_id):
-  delete_all(seedr)
-  add=seedr.addTorrent(MagneticURL)
-#   print(add)
-  if add["result"]==True:
-    table=seedr.listContents()
-    i = 0
-    last_progress = -1
-    while len(table['torrents']) != 0 and i < 30:
-        for torrent in table['torrents']:
-            size = convert_size(torrent['size'])
-            print(f"{torrent['id']}: {torrent['name']}, {size}, {torrent['progress']}%")
-            if last_progress != torrent['progress']:
-                i = 0
-                last_progress = torrent['progress']
-        table = seedr.listContents()
-        sleep(1)
-        i += 1
-        if i==15:
-          seedr.deleteTorrent(torrent['id'])
-          break
+def seedr_download(MagneticURL,seedr):
+    add=seedr.addTorrent(MagneticURL)
+    folder={}
+    folder['id']=0
+    file_urls={}
+    if add["result"]==True:
+        table=seedr.listContents()
+        i = 0
+        last_progress = -1
+        while len(table['torrents']) != 0 and i < 30:
+            for torrent in table['torrents']:
+                size = convert_size(torrent['size'])
+                print(f"{torrent['id']}: {torrent['name']}, {size}, {torrent['progress']}%")
+                if last_progress != torrent['progress']:
+                    i = 0
+                    last_progress = torrent['progress']
+            table = seedr.listContents()
+            sleep(1)
+            i += 1
+            if i==30:
+              seedr.deleteTorrent(torrent['id'])
+              break
 
-    if len(table['folders']) != 0:
-        print("Completed torrents:")
-        for folder in table['folders']:
-            print()
-            size = convert_size(folder['size'])
-            print(f"{folder['id']}: {folder['name']}, {size}")
-            table=seedr.listContents(folder['id'])
-            # seedr.deleteFolder(folder['id'])
-            if len(table['files']) == 0:
-                print("No files in this folder.")
+        if len(table['folders']) != 0:
+            print("Completed torrents:")
+            for folder in table['folders']:
+                print()
+                size = convert_size(folder['size'])
+                print(f"{folder['id']}: {folder['name']}, {size}")
+                table=seedr.listContents(folder['id'])
+                # seedr.deleteFolder(folder['id'])
+                if len(table['files']) == 0:
+                    print("No files in this folder.")
+                else:
+                    print("\tFiles:")
+
+                    for file in table['files']:
+                        print(f"\t{file['folder_file_id']}: {file['name']}, {convert_size(file['size'])}, Video={file['play_video']}")
+                        if file['play_video']==True:
+                          if file['size'] < 2000000000:
+                              link=seedr.fetchFile(file['folder_file_id'])
+                              # print('\t',link["url"])
+                              quoted_link = urllib.parse.unquote(link["url"])
+                              encoded_url = urllib.parse.quote(quoted_link, safe=':/?&=()[]')
+                              # print(encoded_url)
+                              print(file['name'],encoded_url)
+                              file_urls[file['name']] = encoded_url
+                          else:
+                            print(f"File size {convert_size(file['size'])} is Greater than 2GB")
+    return folder['id'],file_urls
+
+def aria2_download(filename, link):
+    print(f"Downloading {filename} with {link}")
+
+    command = f"aria2c -o '{filename}' -c -s 16 -x 16 -k 1M -j 1 '{link}'"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    process.wait()
+    print(f"Download completed for {filename}")
+
+    return filename
+
+def check_bot_status():
+    url = f"{API_SERVER_URL}{TELEGRAM_TOKEN}/getMe"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print("Bot is Working")
+            return True
+        else:
+            os.system(f"nohup ./telegram-bot-api --api-hash {API_HASH} --api-id {API_ID} --local &")
+            sleep(3)
+            response = requests.get(url)
+            if response.status_code == 200:
+                print("Bot started successfully")
+                return True
             else:
-                print("\tFiles:")
-                for file in table['files']:
-                    print(f"\t{file['folder_file_id']}: {file['name']}, {convert_size(file['size'])}, Video={file['play_video']}")
-                    if file['play_video']==True:
-                      if file['size'] / (1024**3) < 1.95:
-                          link=seedr.fetchFile(file['folder_file_id'])
-                          # print('\t',link["url"])
-                          quoted_link = urllib.parse.unquote(link["url"])
-                          encoded_url = urllib.parse.quote(quoted_link, safe=':/?&=()[]')
-                          # print(encoded_url)
-                          aria2_download(file['name'],encoded_url, chat_id)
-                      else:
-                        print(f"File size {convert_size(file['size'])} is Greater than 2GB")
-            seedr.deleteFolder(folder['id'])
+                raise Exception('Failed to start the bot.')
+    except Exception as e:
+        print(e)
+        os.system(f"nohup ./telegram-bot-api --api-hash {API_HASH} --api-id {API_ID} --local &")
+        sleep(3)
+        response = requests.get(url)
+        if response.status_code == 200:
+            print("Bot started successfully")
+            return True
+        else:
+            raise Exception('Failed to start the bot.')
+        return False
