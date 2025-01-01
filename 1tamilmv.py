f52a244405b41b654854cebc9d36bed6541e0d75
thereisnothinginhere <96543392+thereisnothinginhere@users.noreply.github.com>
2025-01-01 17:12:15 +0530

    Update 1tamilmv.py

--- 1tamilmv.py
+++ 1tamilmv.py
@@ -56,77 +56,7 @@ with open(sites_filename, "a") as sites_file:
             for filepath, encoded_url in urls.items():
                 aria2_download(filepath, encoded_url)
                 upload_video(chat_id, c, THUMBNAIL_PATH)
-                os.remove(name: Build Telegram API Server for Windows
-on:
-  workflow_dispatch:
-
-jobs:
-  build:
-    runs-on: windows-latest
-    permissions:
-      contents: write
-
-    steps:
-    - name: Checkout code
-      uses: actions/checkout@v3
-
-    - name: Install Chocolatey
-      run: Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-    - name: Install Visual Studio Build Tools
-      run: choco install visualstudio2022buildtools --package-parameters "--add Microsoft.VisualStudio.Workload.VCTools --includeRecommended --includeOptional"
-
-    - name: Install Dependencies
-      run: choco install -y git cmake
-
-    - name: Clone Repositories and Install Vcpkg
-      run: |
-        git clone --recursive https://github.com/tdlib/telegram-bot-api.git
-        cd telegram-bot-api
-        git clone https://github.com/Microsoft/vcpkg.git
-        cd vcpkg
-        ./bootstrap-vcpkg.bat
-
-    - name: Install x64 Dependencies
-      run: |
-        cd D:/a/windowsbotserver/telegram-bot-api/vcpkg
-        ./vcpkg.exe install gperf:x64-windows openssl:x64-windows zlib:x64-windows
-
-    - name: Build x64 Version
-      run: |
-        cd D:/a/windowsbotserver/telegram-bot-api
-        Remove-Item build -Force -Recurse -ErrorAction SilentlyContinue
-        mkdir build
-        cd build
-        cmake -A x64 -DCMAKE_INSTALL_PREFIX:PATH=.. -DCMAKE_TOOLCHAIN_FILE:FILEPATH=../vcpkg/scripts/buildsystems/vcpkg.cmake ..
-        cmake --build . --target install --config Release
-        mkdir -p ../Windows-x64
-        Copy-Item ../bin/* ../Windows-x64/ -Recurse
-
-    - name: Install x86 Dependencies
-      run: |
-        cd D:/a/windowsbotserver/telegram-bot-api/vcpkg
-        ./vcpkg.exe install gperf:x86-windows openssl:x86-windows zlib:x86-windows
-
-    - name: Build x86 Version
-      run: |
-        cd D:/a/windowsbotserver/telegram-bot-api
-        Remove-Item build -Force -Recurse -ErrorAction SilentlyContinue
-        mkdir build
-        cd build
-        cmake -A Win32 -DCMAKE_INSTALL_PREFIX:PATH=.. -DCMAKE_TOOLCHAIN_FILE:FILEPATH=../vcpkg/scripts/buildsystems/vcpkg.cmake ..
-        cmake --build . --target install --config Release
-        mkdir -p ../Windows-x32
-        Copy-Item ../bin/* ../Windows-x32/ -Recurse
-
-    - name: Commit and Push
-      run: |
-        git config --local user.email "action@github.com"
-        git config --local user.name "GitHub Action"
-        git add telegram-bot-api/Windows-x64/* telegram-bot-api/Windows-x32/*
-        git commit -m "Updated Windows binaries for x64 and x32"
-        git push
-)
+                os.remove(filepath)
             seedr.deleteFolder(id)
         sites_file.write(last_part + "\n")
         sites_file.flush()
